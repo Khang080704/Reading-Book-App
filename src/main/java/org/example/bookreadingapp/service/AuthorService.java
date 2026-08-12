72d6d0dd43ba2f5d0b7064fd9cb58c0e762910fd
Khang080704 <pgkhangt1@gmail.com>
2026-08-12 20:16:56 +0700

    feat: implement FavoriteService to manage user favorites and AuthorService for author data retrieval

--- src/main/java/org/example/bookreadingapp/service/AuthorService.java
+++ src/main/java/org/example/bookreadingapp/service/AuthorService.java
@@ -50,13 +50,16 @@ public class AuthorService {
 
         if(author.isPresent()) {
             AuthorDetail authorDetail = author.get();
-            AuthorDetailDTO dto = new AuthorDetailDTO();
-            dto.setBio(authorDetail.getBio());
-            dto.setBirthDate(authorDetail.getBirthDay());
-            dto.setFullName(authorDetail.getFullName());
-            dto.setCreatedAt(authorDetail.getCreatedAt());
-            dto.setLastModifiedAt(authorDetail.getLastModify());
-            return dto;
+
+            return AuthorDetailDTO.builder()
+                    .bio(authorDetail.getBio())
+                    .birthDate(authorDetail.getBirthDay())
+                    .fullName(authorDetail.getFullName())
+                    .createdAt(authorDetail.getCreatedAt())
+                    .lastModifiedAt(authorDetail.getLastModify())
+                    .avatar("https://covers.openlibrary.org/a/olid/" + olKey + "-M.jpg")
+                    .build();
+
         }
         else {
             log.info("Author detail not found in database forolKey: {}", olKey);
